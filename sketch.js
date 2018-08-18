7eb60288fe2ed70a0ad845ced5067ed6513f816c
sllentProgrammer <ville.backander@gmail.com>
2018-08-18 14:32:46 +0200

    AIGame project
    AI game in p5js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,58 @@
+var x = 600/2;
+var y = 600/2;
 
+var foodx;
+var foody;
+
+var selectPos = true;
+var cheats = false;
+
+var button;
+var framerate = frameRate;
 
 function setup() {
-  
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(244, 248, 252);
+
+  update();
+  show();
+  showFood();
+  frameRate(60);
+
+//////////////////////////////////////////
+
+if(cheats == false) {
+  button = createButton('turn cheats on');
+  button.position(0, 20);
+  button.size(100, 50);
+  button.mousePressed(setCheats);
+} else {
+  button = createButton('turn cheats off');
+  button.position(0, 20);
+  button.size(100, 50);
+  button.mousePressed(setCheats);
+}
+
+///////////////////////////////////////
+
+  if(selectPos == true) {
+    foodx = random(600);
+    foody = random(600);
+    selectPos = false;
+  }
+
+  number = floor(random(5));
+}
+
+/////////////////////////////////////
+
+function setCheats() {
+  if(cheats == false) {
+    cheats = true;
+  } else {
+    cheats = false;
+  }
 }
